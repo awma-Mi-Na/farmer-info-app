120d59ca53b59481f324aa87e78f14ca86f2dcdd
awma-Mi-Na <awmach.9@gmail.com>
2022-01-13 14:01:57 +0530

    district filter by name

--- app/Models/District.php
+++ app/Models/District.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,12 @@ class District extends Model
     public function markets()
     {
         return $this->hasMany(Market::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        $query->when($filters['name'] ?? false, function (Builder $query, string $district) {
+            $query->where('name', 'like', '%' . $district . '%');
+        });
     }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,6 @@
 
 namespace App\Models;
 
-use DB;
-use Doctrine\DBAL\Query\QueryBuilder;
-use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
